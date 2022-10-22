aae28baed9d7a91d5946da402f0fe35c54ad937b
jheave <jimenezjheave123@gmail.com>
2022-10-22 20:07:06 +0800

    testing husky pre commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
 function App({ Component, pageProps }) {
+  var testing = 0;
   return (
     <ChakraProvider>
       <Component {...pageProps} />
