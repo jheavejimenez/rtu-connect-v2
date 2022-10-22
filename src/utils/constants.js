3fbf229f3f184122bf5522feef95bfe2c6f728e8
jheave <jimenezjheave123@gmail.com>
2022-10-22 22:01:47 +0800

    add utils

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+import { chain } from 'wagmi';
+
+// lens protocol
+export const API_URL = 'https://api-mumbai.lens.dev/';
+
+// alchemy
+export const ALCHEMY_KEY = process.env.REACT_APP_ALCHEMY_KEY;
+export const ALCHEMY_RPC = `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`;
+
+// polygon
+export const POLYGON_MUMBAI = {
+  ...chain.polygonMumbai,
+  name: 'Polygon Mumbai',
+  rpcUrls: { default: 'https://rpc-mumbai.maticvigil.com' }
+};
+
+export const CHAIN_ID = POLYGON_MUMBAI.id;
