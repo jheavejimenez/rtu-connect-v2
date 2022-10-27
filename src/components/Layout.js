2438a9e9efd50e8c9b274beadb479abc1f72157e
jheave <jimenezjheave123@gmail.com>
2022-10-27 12:24:14 +0800

    add logic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,20 @@
 import Head from 'next/head';
 import { useAccount, useDisconnect, useNetwork } from 'wagmi';
 
+import useIsMounted from '../utils/hooks/useIsMounted';
+
 function Layout({ children }) {
+  const { mounted } = useIsMounted();
+
   // user wallet
   const { address, isDisconnected } = useAccount();
   const { chain } = useNetwork();
   const { disconnect } = useDisconnect();
+
+  const clearAuthState = () => {
+    setProfileId(null);
+    setCurrentProfile(null);
+  };
 
   return (
     <>
