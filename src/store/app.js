0b076c64f80ca2c179084939e9d8ba1a00480443
jheave <jimenezjheave123@gmail.com>
2022-10-27 12:38:51 +0800

    add useAppPersistStore

--- src/store/app.js
+++ src/store/app.js
@@ -1,10 +1,23 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export const useAppStore = create((set) => ({
-  profiles: [],
-  setProfiles: (profiles) => set({ profiles }),
+  profile: [],
+  setProfiles: (profile) => set({ profile }),
   currentProfile: null,
   setCurrentProfile: (currentProfile) => set({ currentProfile }),
   userSigNonce: 0,
   setUserSigNonce: (userSigNonce) => set(() => ({ userSigNonce }))
 }));
+
+export const useAppPersistStore = create(
+  persist(
+    (set) => ({
+      profileId: null,
+      setProfileId: (profileId) => set(() => ({ profileId })),
+      notificationCount: 0,
+      setNotificationCount: (notificationCount) => set(() => ({ notificationCount }))
+    }),
+    { name: 'rtuconnect.store' }
+  )
+);
