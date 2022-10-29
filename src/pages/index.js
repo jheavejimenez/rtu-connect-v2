1a9f3d232393e0571f6a907ea6c92b4c6d10baaa
jheave <jimenezjheave123@gmail.com>
2022-10-29 20:00:53 +0800

    style: add container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
-import { Container, Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 import { useAppStore } from '../store/app';
 
 function Home() {
   const currentProfile = useAppStore((state) => state.currentProfile);
   return (
-    <Container mx="auto">
+    <div className="container mx-auto max-w-screen-xl flex-grow py-8 px-0 sm:px-5">
       <Grid column="12">
         <GridItem colSpan="12" bg="orange.300" area={'header'} mb="5">
           {currentProfile ? <>Home feed</> : <>Explore</>}
         </GridItem>
       </Grid>
-    </Container>
+    </div>
   );
 }
 
