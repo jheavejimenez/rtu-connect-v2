f5a925ed31483c4cd1dab6d4c43cbb4aed2fc92a
jheave <jimenezjheave123@gmail.com>
2022-11-02 23:01:52 +0800

    fix: bug in image avatar

--- src/components/Navbar/Avatar.js
+++ src/components/Navbar/Avatar.js
@@ -15,7 +15,6 @@ function Avatar() {
           as={'img'}
           className={'inline-block h-10 w-10 rounded-full ring-2 ring-blue-800'}
           alt={'avatar'}
-          // check if profileId.picture exists if not use logoProfile
           src={
             'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
           }
