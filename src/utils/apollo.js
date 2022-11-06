b02bbde7814a4765a446fd7d17578bb5e588f30b
jheave <jimenezjheave123@gmail.com>
2022-11-07 00:36:15 +0800

    feat: refresh token to get and store a new pair of access and refresh tokens

--- src/utils/apollo.js
+++ src/utils/apollo.js
@@ -1,4 +1,5 @@
-import { ApolloClient, ApolloLink, fromPromise, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, fromPromise, HttpLink, InMemoryCache, toPromise } from '@apollo/client';
+import axios from 'axios';
 
 import { API_URL } from './constants';
 import { clearLocalStorage, isTokenExpired } from './helpers';
@@ -29,7 +30,34 @@ const authLink = new ApolloLink((operation, forward) => {
     return forward(operation);
   }
   // this is where we get the new access token
-  return fromPromise();
+  return fromPromise(
+    axios(API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify({
+        variables: {
+          request: { refreshToken: localStorage.getItem('refreshToken') }
+        }
+      })
+    })
+      .then(({ data }) => {
+        const accessToken = data?.data?.refresh?.accessToken;
+        const refreshToken = data?.data?.refresh?.refreshToken;
+        operation.setContext({
+          headers: {
+            'x-access-token': `Bearer ${accessToken}`
+          }
+        });
+
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+
+        return toPromise(forward(operation));
+      })
+      .catch(() => {
+        return toPromise(forward(operation));
+      })
+  );
 });
 
 const client = new ApolloClient({
