160804817997dba9451b5d62f1fdecead3fa9790
jheave <jimenezjheave123@gmail.com>
2022-11-09 00:53:31 +0800

    feat: wallet options

--- src/components/Navbar/LoginBtn.js
+++ src/components/Navbar/LoginBtn.js
@@ -1,3 +1,4 @@
+import { ConnectKitButton } from 'connectkit';
 import { useState } from 'react';
 
 import Modal from '../UI/Modal';
@@ -11,15 +12,16 @@ function LoginBtn() {
       <Modal title={'Connect your wallet'} isOpen={showModal} isClose={() => setShowModal(false)}>
         <Login />
       </Modal>
-      <button
-        className={
-          'inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white' +
-          ' bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
-        }
-        onClick={() => setShowModal(!showModal)}
-      >
-        {'Login'}
-      </button>
+      {/*<button*/}
+      {/*  className={*/}
+      {/*    'inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white' +*/}
+      {/*    ' bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'*/}
+      {/*  }*/}
+      {/*  onClick={() => setShowModal(!showModal)}*/}
+      {/*>*/}
+      {/*  {'Login'}*/}
+      {/*</button>*/}
+      <ConnectKitButton />
     </>
   );
 }
--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,31 +1,41 @@
 import { ApolloProvider } from '@apollo/client';
+import { ConnectKitProvider } from 'connectkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
 import client from '../utils/apollo';
-import { ALCHEMY_KEY } from '../utils/constants';
+import { ALCHEMY_KEY, ALCHEMY_RPC, CHAIN_ID } from '../utils/constants';
 
 const { chains, provider } = configureChains(
   [chain.polygonMumbai],
   [alchemyProvider({ apiKey: ALCHEMY_KEY })]
 );
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: [
+const connectors = () => {
+  return [
     new MetaMaskConnector({
       chains,
       options: { shimDisconnect: true }
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: { rpc: { [CHAIN_ID]: ALCHEMY_RPC } }
     })
-  ],
+  ];
+};
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
   provider
 });
 
 function Providers({ children }) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <ApolloProvider client={client}>{children}</ApolloProvider>
+      <ConnectKitProvider>
+        <ApolloProvider client={client}>{children}</ApolloProvider>
+      </ConnectKitProvider>
     </WagmiConfig>
   );
 }
