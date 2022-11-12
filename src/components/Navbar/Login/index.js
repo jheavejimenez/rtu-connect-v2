2c2e41b9a84ff47d159fb3de94025edadc62a134
jheave <jimenezjheave123@gmail.com>
2022-11-13 01:26:16 +0800

    feat: persist user data

--- src/components/Navbar/Login/index.js
+++ src/components/Navbar/Login/index.js
@@ -3,13 +3,19 @@ import { ConnectKitButton } from 'connectkit';
 import { useAccount, useSignMessage } from 'wagmi';
 
 import { AUTHENTICATION, GET_CHALLENGE } from '../../../graphQL/auth/auth-mutations';
+import { GET_PROFILES } from '../../../graphQL/queries/get-profiles';
+import { useAppStore } from '../../../store/app';
 import { setLocalStorage } from '../../../utils/helpers';
 import Modal from '../../UI/Modal';
 
 function Login() {
+  const setProfiles = useAppStore((state) => state.setProfiles);
+  const setCurrentProfile = useAppStore((state) => state.setCurrentProfile);
+
   const [getChallenge, { error: challengeError, loading: challengeLoading }] = useLazyQuery(GET_CHALLENGE);
   const [authenticate, { error: authenticateError, loading: authenticateLoading }] =
     useMutation(AUTHENTICATION);
+  const [getProfile, { error: profileError, loading: profileLoading }] = useLazyQuery(GET_PROFILES);
 
   const { connector, isConnected, address } = useAccount();
   const { signMessageAsync, isLoading: signLoading } = useSignMessage({
@@ -39,6 +45,16 @@ function Login() {
         ['accessToken', 'refreshToken'],
         [auth.data?.authenticate.accessToken, auth.data?.authenticate.refreshToken]
       );
+      const { data: profileData } = await getProfile({
+        variables: { request: { ownedBy: address } }
+      });
+
+      if (profileData.profiles?.items) {
+        const profiles = profileData?.profiles.items;
+        const currentProfile = profiles[0];
+        setProfiles(profiles);
+        setCurrentProfile(currentProfile);
+      }
     } catch (error) {
       console.log(error); // TODO: convert it to toast message
     }
