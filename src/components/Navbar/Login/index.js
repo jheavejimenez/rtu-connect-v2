49dee43aa1409e7faabb29eb1046a7f863eb73b0
jheave <jimenezjheave123@gmail.com>
2022-11-13 00:58:54 +0800

    feat: lens api authentication

--- src/components/Navbar/Login/index.js
+++ src/components/Navbar/Login/index.js
@@ -1,24 +1,52 @@
+import { useLazyQuery, useMutation } from '@apollo/client';
 import { ConnectKitButton } from 'connectkit';
-import { useAccount, useDisconnect } from 'wagmi';
+import { useAccount, useSignMessage } from 'wagmi';
 
+import { AUTHENTICATION, GET_CHALLENGE } from '../../../graphQL/auth/auth-mutations';
+import { setLocalStorage } from '../../../utils/helpers';
 import Modal from '../../UI/Modal';
 
-function Dialog(props) {
-  return null;
-}
-
 function Login() {
-  const { connector, isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
+  const { connector, isConnected, address } = useAccount();
+  const [challenge, { error: challengeError, loading: challengeLoading }] = useLazyQuery(GET_CHALLENGE);
+  const [authenticate, { error: authenticateError, loading: authenticateLoading }] =
+    useMutation(AUTHENTICATION);
+  const { signMessageAsync, isLoading: signLoading } = useSignMessage({
+    onError: (error) => {
+      console.log('error signing message', error);
+    }
+  });
 
-  // function handleLogin() {
-  //   try {
-  //     const getChallenge =
-  //   }
-  // }
+  async function handleLogin() {
+    try {
+      const getChallenge = await challenge({
+        variables: { request: { address } }
+      });
+      if (!getChallenge?.data?.challenge?.text) {
+        throw new Error('No challenge found');
+      }
+
+      const signature = await signMessageAsync({
+        message: challenge?.data?.challenge?.text
+      });
+
+      const auth = await authenticate({
+        variables: { request: { address, signature } }
+      });
+      setLocalStorage(
+        ['accessToken', 'refreshToken'],
+        auth.data?.authenticate.accessToken,
+        auth.data?.authenticate.refreshToken
+      );
+      console.log('auth', auth.data?.authenticate.accessToken);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return connector?.id ? (
     <Modal isOpen={isConnected} isClose={() => false} title={'Login to RTU Connect'}>
-      <button onClick={() => disconnect()}>{'Disconnect'}</button>
+      <button onClick={() => handleLogin()}>{'Disconnect'}</button>
     </Modal>
   ) : (
     <div className={'p-5'}>
