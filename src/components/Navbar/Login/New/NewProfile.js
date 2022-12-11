333470b1595d4a657553e18b45c1d4253ed88a13
jheave <jimenezjheave123@gmail.com>
2022-12-11 20:41:33 +0800

    UI: create new profile

--- src/components/Navbar/Login/New/NewProfile.js
+++ src/components/Navbar/Login/New/NewProfile.js
@@ -1,38 +1,51 @@
+import { useFormik } from 'formik';
 import { useState } from 'react';
 import { useAccount } from 'wagmi';
+
+import Button from '../../../UI/Button';
 
 function NewProfile({ isModal = false }) {
   const [avatar, setAvatar] = useState('');
   const [uploading, setUploading] = useState(false);
   const { address } = useAccount();
 
+  const formik = useFormik({
+    initialValues: {
+      handle: ''
+    },
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
   return (
-    <form className={'space-y-4'}>
-      {isModal && (
-        <div className={'mb-2 space-y-4'}>
-          <img className={'w-10 h-10'} height={40} width={40} alt={'Logo'} />
-          <div className={'text-xl font-bold'}>{'Signup to'}</div>
+    <form onSubmit={formik.handleSubmit}>
+      <div className={'flex flex-col p-12'}>
+        <div className={'flex flex-col items-center justify-center'}>
+          <label
+            className={
+              'flex flex-col items-center justify-center w-32 h-32 border-2 border-gray-300 rounded-full cursor-pointer'
+            }
+          >
+            {'Avatar'}
+          </label>
         </div>
-      )}
-      <input label={'Handle'} type={'text'} placeholder={'gavin'} />
-      <div className={'space-y-1.5'}>
-        <div className={'label'}>{'Avatar'}</div>
-        <div className={'space-y-3'}>
-          {avatar && (
-            <div>
-              <img className={'w-60 h-60 rounded-lg'} height={240} width={240} src={avatar} alt={avatar} />
-            </div>
-          )}
-          <div>
-            <div className={'flex items-center space-x-3'}>
-              <input />
-            </div>
-          </div>
-        </div>
+        <input
+          id={'handle'}
+          name={'handle'}
+          type={'text'}
+          placeholder={'@Jheave'}
+          onChange={formik.handleChange}
+          value={formik.values.handle}
+          className={
+            'px-4 py-2 my-4 border border-gray-300' +
+            ' rounded-md shadow-sm focus:outline-none' +
+            ' focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+          }
+        />
+        <Button type={'submit'}>{'Create'}</Button>
       </div>
-      <button className={'ml-auto'} type={'submit'}>
-        {'Signup'}
-      </button>
     </form>
   );
 }
