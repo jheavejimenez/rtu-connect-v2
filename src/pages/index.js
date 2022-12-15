81e05b732e98673e63ebd5b8946d1948537b82d2
jheave <jimenezjheave123@gmail.com>
2022-12-15 17:51:55 +0800

    deps: add interweave

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import { useAppStore } from '../store/app';
 
 function Home() {
   const currentProfile = useAppStore((state) => state.currentProfile);
-  return <h1>{'hello'}</h1>;
 }
 
 export default Home;
