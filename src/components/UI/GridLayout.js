2fb896af204ca187309ee5975a93652d3c38965c
jheave <jimenezjheave123@gmail.com>
2022-12-17 20:44:22 +0800

    component: grid layout

--- src/components/UI/GridLayout.js
+++ src/components/UI/GridLayout.js
@@ -1,9 +1,11 @@
-function GridLayout({ children }) {
+export const GridLayout = ({ children }) => {
   return (
-    <div className={'mx-auto max-w-screen-xl flex-grow pt-8 pb-2 px-0'}>
+    <div className={'mx-auto max-w-screen-xl flex-grow pt-20 pb-2 px-0'}>
       <div className={'grid grid-cols-12'}>{children}</div>
     </div>
   );
-}
+};
 
-export default GridLayout;
+export const GridItem = ({ children, span }) => {
+  return <div className={`lg:col-span-${span} md:col-span-12 col-span-12`}>{children}</div>;
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
+import { GridItem, GridLayout } from '../components/UI/GridLayout';
 import { useAppStore } from '../store/app';
 
 function Home() {
   const currentProfile = useAppStore((state) => state.currentProfile);
+
+  return (
+    <GridLayout>
+      <GridItem span={12}>
+        <h1 className={'text-3xl font-bold text-center'}>{'Welcome to Next.js!'}</h1>
+      </GridItem>
+    </GridLayout>
+  );
 }
 
 export default Home;
