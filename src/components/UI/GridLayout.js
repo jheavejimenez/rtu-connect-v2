9529e974dfc522097d548e19d3e17e84b1b591e8
jheave <jimenezjheave123@gmail.com>
2022-12-17 20:47:01 +0800

    refactor

--- src/components/UI/GridLayout.js
+++ src/components/UI/GridLayout.js
@@ -1,11 +1,11 @@
-export const GridLayout = ({ children }) => {
+export const GridLayout = ({ children, classname }) => {
   return (
-    <div className={'mx-auto max-w-screen-xl flex-grow pt-20 pb-2 px-0'}>
+    <div className={`mx-auto max-w-screen-xl flex-grow pt-20 pb-2 px-0 ${classname}`}>
       <div className={'grid grid-cols-12'}>{children}</div>
     </div>
   );
 };
 
-export const GridItem = ({ children, span }) => {
-  return <div className={`lg:col-span-${span} md:col-span-12 col-span-12`}>{children}</div>;
+export const GridItem = ({ children, classname }) => {
+  return <div className={`lg:col-span-12} md:col-span-12 col-span-12 ${classname}`}>{children}</div>;
 };
