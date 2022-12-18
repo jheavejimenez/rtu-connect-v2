ef48b8fc22829cfae25397dee26107254e75baff
jheave <jimenezjheave123@gmail.com>
2022-12-18 09:51:01 +0800

    refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,7 @@ function Home() {
 
   return (
     <GridLayout>
-      <GridItem>
-        <h1 className={'text-3xl font-bold text-center'}>{'Welcome to Next.js!'}</h1>
-      </GridItem>
+      <GridItem />
     </GridLayout>
   );
 }
