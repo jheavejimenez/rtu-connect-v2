bbcb9571f214a6a582235cea486716fd8774514e
jheave <jimenezjheave123@gmail.com>
2022-12-22 12:18:38 +0800

    feat: publication

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,7 @@ import { useAppStore } from '../store/app';
 function Home() {
   const currentProfile = useAppStore((state) => state.currentProfile);
 
-  return (
-    <GridLayout>
-      <h1>{'test '}</h1>
-    </GridLayout>
-  );
+  return <GridLayout />;
 }
 
 export default Home;
