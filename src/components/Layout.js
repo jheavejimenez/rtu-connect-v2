1796714383419302ba12c030b10a6f56f1fbafdf
jheave <jimenezjheave123@gmail.com>
2023-01-04 23:26:13 +0800

    improve ui color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -89,7 +89,7 @@ function Layout({ children }) {
           }
         }}
       />
-      <div className={'flex flex-col min-h-screen'}>
+      <div className={'flex flex-col min-h-screen bg-gray-50'}>
         <Navbar />
         {children}
       </div>
