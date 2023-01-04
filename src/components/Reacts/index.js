8d1b8230a7eac71efaa8873b009dc40421e51d88
jheave <jimenezjheave123@gmail.com>
2023-01-05 00:21:41 +0800

    feat: react

---
+++ src/components/Reacts/index.js
@@ -0,0 +1,11 @@
+import Like from './Like';
+
+function Reactions() {
+  return (
+    <span className={'justify-center items-center flex gap-6 pt-3 -ml-2 text-gray-500 sm:gap-8'}>
+      <Like />
+    </span>
+  );
+}
+
+export default Reactions;
