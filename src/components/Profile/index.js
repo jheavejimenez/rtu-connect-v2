45bdd78c7862e7f52cc139c68251afa13858b3b7
jheave <jimenezjheave123@gmail.com>
2023-01-05 00:03:46 +0800

    feat: user profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,30 +1,28 @@
-import { CheckCircleIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
 
 function UserProfile({ profile }) {
+  console.log(profile);
   return (
     <div className={'flex justify-between items-center'}>
-      <div className={'flex items-center space-x-3'}>
-        <img
-          src={
-            'https://media.lenster.xyz/tr:n-avatar,tr:di-placeholder.webp/https://statics-mumbai-lens-staging.s3.eu-west-1.amazonaws.com/profile/QmVeEwimhwaebeHFDTVY3XNjFuaNUWuhv1ksNefnzeTKXH'
-          }
-          loading={'lazy'}
-          className={('w-10 h-10', 'bg-gray-200 rounded-full border')}
-          height={40}
-          width={40}
-          alt={profile?.handle}
-        />
-        <div>
-          <div className={'flex gap-1 items-center max-w-sm truncate'}>
-            <div className={'text-md'}>{profile?.name ?? profile?.handle}</div>
-            <CheckCircleIcon className={'w-4 h-4 text-blue-700'} />
+      <Link href={`/u/${profile?.handle}`}>
+        <div className={'flex items-center space-x-3'}>
+          <img
+            src={'https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg'}
+            loading={'lazy'}
+            className={('w-12 h-12', 'bg-gray-200 rounded-full border')}
+            height={40}
+            width={40}
+            alt={profile?.handle}
+          />
+          <div>
+            <div className={'flex gap-1 items-center max-w-sm truncate'}>
+              <div className={'text-md'}>{profile?.name ?? profile?.handle}</div>
+              <CheckCircleIcon className={'w-4 h-4 text-blue-700'} />
+            </div>
           </div>
-          <p className={'text-sm'}>
-            {profile?.handle}
-            {'@'}
-          </p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/components/Publication/SinglePublication.js
+++ src/components/Publication/SinglePublication.js
@@ -1,6 +1,7 @@
 import moment from 'moment/moment';
 import { useRouter } from 'next/router';
 
+import UserProfile from '../Profile';
 import PublicationBody from './PublicationBody';
 
 function SinglePublication({ publication, feedItem }) {
@@ -8,11 +9,7 @@ function SinglePublication({ publication, feedItem }) {
   const isMirror = publication.__typename === 'Mirror';
   const firstComment = feedItem?.comments && feedItem.comments[0];
   const rootPublication = feedItem ? (firstComment ? firstComment : feedItem?.id) : publication;
-  const profile = feedItem
-    ? rootPublication.profile
-    : isMirror
-    ? publication?.mirrorOf?.profile
-    : publication?.profile;
+  const profile = feedItem.profile;
   const timestamp = feedItem
     ? rootPublication.createdAt
     : isMirror
@@ -23,8 +20,7 @@ function SinglePublication({ publication, feedItem }) {
     <article className={'hover:bg-gray-100 cursor-pointer first:rounded-t-xl last:rounded-b-xl p-5'}>
       <div className={'flex justify-between pb-4 space-x-1.5'}>
         <span onClick={(event) => event.stopPropagation()}>
-          {'user profile'}
-          {/*<UserProfile profile={profile ?? publication?.collectedBy?.defaultProfile} />*/}
+          <UserProfile profile={profile ?? publication?.collectedBy?.defaultProfile} />
         </span>
         <span className={'text-xs text-gray-500'}>{moment(timestamp).fromNow()}</span>
       </div>
