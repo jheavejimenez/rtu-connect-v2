908e3d33700e69220be0abea1ae23ac7cd3a56d4
jheave <jimenezjheave123@gmail.com>
2023-01-04 19:41:45 +0800

    design: fix layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,12 @@ import { useAppStore } from '../store/app';
 
 function Home() {
   const currentProfile = useAppStore((state) => state.currentProfile);
-
-  return <GridLayout />;
+  return (
+    <GridLayout>
+      <div className={'space-y-5 lg:col-span-8 md:col-span-12 col-span-12 mb-5'}>{'newsfeed here'}</div>
+      <div className={'lg:col-span-4 md:col-span-12 col-span-12'}>{'beta warning here'}</div>
+    </GridLayout>
+  );
 }
 
 export default Home;
