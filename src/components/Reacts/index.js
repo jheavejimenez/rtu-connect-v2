00dfa9132c5d1078933c6a1f7b46c7b9708de461
jheave <jimenezjheave123@gmail.com>
2023-01-06 08:32:21 +0800

    feat: share

--- src/components/Reacts/index.js
+++ src/components/Reacts/index.js
@@ -1,11 +1,13 @@
 import Comment from './Comment';
 import Like from './Like';
+import Share from './Share';
 
 function Reactions() {
   return (
     <span className={'justify-center items-center flex gap-6 pt-3 -ml-2 text-gray-500 sm:gap-8'}>
       <Like />
       <Comment />
+      <Share />
     </span>
   );
 }
