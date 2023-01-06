0bb8bff99380390bf5a0e460544731e1187936f1
jheave <jimenezjheave123@gmail.com>
2023-01-06 08:36:42 +0800

    feat: infinite scroll

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -1,21 +1,33 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import { publications } from '../../utils/helpers';
 import SinglePublication from '../Publication/SinglePublication';
+import Spinner from '../UI/Spinner';
 
 function Feed() {
+  const loadMore = async () => {};
   /*
    * note the current publication data is hard coded
    * need to replace with real data
    */
   return (
-    <div className={'divide-y-[1px] rounded-none sm:rounded-xl border bg-white'}>
-      {publications?.map((publication, index) => (
-        <SinglePublication
-          key={`${publication?.id}_${index}`}
-          feedItem={publication}
-          publication={publication}
-        />
-      ))}
-    </div>
+    <InfiniteScroll
+      dataLength={publications?.length ?? 0}
+      scrollThreshold={50}
+      hasMore={false}
+      next={loadMore}
+      loader={<Spinner />}
+    >
+      <div className={'divide-y-[1px] rounded-none sm:rounded-xl border bg-white'}>
+        {publications?.map((publication, index) => (
+          <SinglePublication
+            key={`${publication?.id}_${index}`}
+            feedItem={publication}
+            publication={publication}
+          />
+        ))}
+      </div>
+    </InfiniteScroll>
   );
 }
 
