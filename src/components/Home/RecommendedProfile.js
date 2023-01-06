15b673f09544006eb146d5442aee85eb32e8f392
jheave <jimenezjheave123@gmail.com>
2023-01-06 23:17:26 +0800

    refactor

--- src/components/Home/RecommendedProfile.js
+++ src/components/Home/RecommendedProfile.js
@@ -1,33 +1,22 @@
-import { EllipsisHorizontalCircleIcon } from '@heroicons/react/24/outline';
-
+import DummyRecommendedProfiles from '../../utils/DummyRecommendedProfile.json';
+import UserProfile from '../Profile';
 import Card from '../UI/Card';
 
-function Suggested() {
-  return null;
-}
-
 function RecommendedProfile() {
+  const profiles = DummyRecommendedProfiles;
   return (
     <Card className={'!rounded-xl'}>
       <h3 className={'font-bold p-3 text-gray-800'}>{'Who to follow'}</h3>
       <aside>
         <div className={'space-y-4 p-5'}>
           {/*<ErrorMessage title="Failed to load recommendations" error={error} />*/}
-          {/*{data?.recommendedProfiles?.slice(0, 5)?.map((profile) => (*/}
-          {/*  <div key={profile?.id} className="truncate">*/}
-          {/*    <UserProfile profile={profile as Profile} isFollowing={profile.isFollowedByMe} showFollow />*/}
-          {/*  </div>*/}
+          {profiles.data?.recommendedProfiles?.slice(0, 5)?.map((profile) => (
+            <div key={profile?.id} className={'truncate'}>
+              <UserProfile profile={profile} isFollowing={profile.isFollowedByMe} showFollow />
+            </div>
+          ))}
           {/*))}*/}
         </div>
-        <button
-          className={
-            'bg-gray-50 hover:bg-gray-100 border-t text-sm w-full rounded-b-xl text-left px-5 py-3 flex items-center space-x-2 text-gray-600'
-          }
-          type={'button'}
-        >
-          <EllipsisHorizontalCircleIcon className={'h-4 w-4'} />
-          <span>{'Show more'}</span>
-        </button>
       </aside>
     </Card>
   );
