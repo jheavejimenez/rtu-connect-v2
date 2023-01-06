16194d424e197df7ed5588e01ee02e22abf4ec66
jheave <jimenezjheave123@gmail.com>
2023-01-06 09:05:06 +0800

    test: add dummy data

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -1,6 +1,6 @@
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { publications } from '../../utils/helpers';
+import dummyData from '../../utils/dummyData';
 import SinglePublication from '../Publication/SinglePublication';
 import Spinner from '../UI/Spinner';
 
@@ -10,7 +10,8 @@ function Feed() {
    * note the current publication data is hard coded
    * need to replace with real data
    */
-  console.log(publications.items);
+  const publications = dummyData.data.explorePublications.items;
+  console.log(publications);
   return (
     <InfiniteScroll
       dataLength={publications?.length ?? 0}
@@ -20,7 +21,7 @@ function Feed() {
       loader={<Spinner />}
     >
       <div className={'divide-y-[1px] rounded-none sm:rounded-xl border bg-white'}>
-        {publications.items?.map((publication, index) => (
+        {publications?.map((publication, index) => (
           <SinglePublication
             key={`${publication?.id}_${index}`}
             feedItem={publication}
