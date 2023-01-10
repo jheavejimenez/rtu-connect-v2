5d06c1ae46f09c60501f29f56ded068ce9b65910
jheave <jimenezjheave123@gmail.com>
2023-01-10 22:42:21 +0800

    gql: explore feed query

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -1,16 +1,28 @@
+import { useQuery } from '@apollo/client';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+import { EXPLORE_FEED } from '../../graphQL/queries/explore-feed';
 import dummyData from '../../utils/dummyData';
 import SinglePublication from '../Publication/SinglePublication';
 import Empty from '../UI/Empty';
 import Spinner from '../UI/Spinner';
 
 function Feed() {
+  const request = {
+    profileId: '0x492a'
+  };
+
+  const { data, loading, error, fetchMore } = useQuery(EXPLORE_FEED, {
+    variables: {
+      feedRequest: {
+        profileId: '0x492a',
+        limit: 10
+      }
+    }
+  });
+  console.log(data);
   const loadMore = async () => {};
-  /*
-   * note the current publication data is hard coded
-   * need to replace with real data
-   */
+
   const publications = dummyData.data.explorePublications.items;
   if (publications?.length === 0) {
     return <Empty message={"You don't follow anyone. Start posting now!"} />;
