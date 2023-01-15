c197468fbdfee83be21d6e6ff5f5fac132f293fa
jheave <jimenezjheave123@gmail.com>
2023-01-15 18:54:50 +0800

    gql: explore-feed

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -1,20 +1,13 @@
 import { useQuery } from '@apollo/client';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { EXPLORE_FEED } from '../../graphQL/queries/profile-feed';
+import { EXPLORE_FEED } from '../../graphQL/queries/explore-feed';
 import SinglePublication from '../Publication/SinglePublication';
 import Empty from '../UI/Empty';
 import Spinner from '../UI/Spinner';
 
 function Feed() {
-  const { data, loading, error, fetchMore } = useQuery(EXPLORE_FEED, {
-    variables: {
-      feedRequest: {
-        profileId: '0x492a',
-        limit: 10
-      }
-    }
-  });
+  const { data, loading, error, fetchMore } = useQuery(EXPLORE_FEED);
   const loadMore = async () => {};
 
   const publications = data?.feed?.items;
