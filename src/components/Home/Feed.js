57c797232573755f9a0a930650fefbef060be12e
jheave <jimenezjheave123@gmail.com>
2023-01-16 21:18:27 +0800

    feat: loadmore SCROLL_THRESHOLD

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -2,7 +2,6 @@ import { useQuery } from '@apollo/client';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 import { EXPLORE_FEED } from '../../graphQL/queries/explore-feed';
-import { SCROLL_THRESHOLD } from '../../utils/constants';
 import SinglePublication from '../Publication/SinglePublication';
 import Empty from '../UI/Empty';
 import Spinner from '../UI/Spinner';
@@ -12,7 +11,7 @@ function Feed() {
     sortCriteria: 'TOP_COMMENTED',
     publicationTypes: ['POST', 'COMMENT', 'MIRROR'],
     noRandomize: true,
-    limit: 10
+    limit: 20
   };
 
   const { data, fetchMore } = useQuery(EXPLORE_FEED, {
@@ -25,17 +24,18 @@ function Feed() {
 
   const loadMore = async () => {
     await fetchMore({
-      variables: { request: { ...explorePublicationsRequest, cursor: pageInfo?.next } }
+      variables: { request: { explorePublicationsRequest, cursor: pageInfo?.next } }
     });
   };
 
   if (publications?.length === 0) {
     return <Empty message={"You don't follow anyone. Start posting now!"} />;
   }
+
   return (
     <InfiniteScroll
       dataLength={publications?.length ?? 0}
-      scrollThreshold={SCROLL_THRESHOLD}
+      scrollThreshold={0.5}
       hasMore={hasMore}
       next={loadMore}
       loader={
