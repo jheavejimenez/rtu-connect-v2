5dd552840f7b7d268081d1791da40b558008bc8f
jheave <jimenezjheave123@gmail.com>
2023-01-17 19:50:53 +0800

    feat: view publication using id

--- src/pages/posts/[id]/index.js
+++ src/pages/posts/[id]/index.js
@@ -1,7 +1,6 @@
 import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 
-import ViewComment from '../../../components/Comment';
 import UserProfile from '../../../components/Profile';
 import Publication from '../../../components/Publication/Publication';
 import Card from '../../../components/UI/Card';
@@ -15,17 +14,37 @@ function ViewPublication() {
     query: { id }
   } = useRouter();
 
-  const { data } = useQuery(GET_PUBLICATION);
+  const { data, loading, error } = useQuery(GET_PUBLICATION, {
+    variables: {
+      publicationRequest: {
+        publicationId: id
+      },
+      profileId: currentProfile?.id ?? null
+    },
+    skip: !id
+  });
 
   const publication = data?.publication;
+
+  if (loading || !data) {
+    return <div>{'Loading...'}</div>;
+  }
+
+  if (error) {
+    return <div>{'Error'}</div>;
+  }
+
+  if (!publication) {
+    return <div>{'No publication found'}</div>;
+  }
 
   return (
     <GridLayout>
       <div className={'space-y-5 lg:col-span-8 md:col-span-12 col-span-12 mb-5'}>
-        <Card className={'divide-y-[1px]'}>
+        <Card className={'!rounded-xl'}>
           <Publication publication={publication} />
         </Card>
-        <ViewComment publication={publication} />
+        {/*<ViewComment publication={publication} />*/}
       </div>
       <div className={'space-y-5 lg:col-span-4 md:col-span-12 col-span-12'}>
         <aside className={'p-5 divide-y-[1px] rounded-none sm:rounded-xl border bg-white'}>
@@ -33,7 +52,6 @@ function ViewPublication() {
             profile={
               publication.__typename === 'Mirror' ? publication?.mirrorOf?.profile : publication?.profile
             }
-            showBio
           />
         </aside>
       </div>
