48ab4219bb3659e195d2a9a6c41729adb25ebdb8
Jhv <jimenezjheave123@gmail.com>
2023-01-30 20:10:33 +0800

    feat: shimmers

--- src/components/Shimmer/ProfileShimmer.js
+++ src/components/Shimmer/ProfileShimmer.js
@@ -2,10 +2,10 @@ function ProfileShimmer() {
   return (
     <div className={'flex justify-between items-center py-1'}>
       <div className={'flex items-center space-x-3'}>
-        <div className={'w-10 h-10 rounded-full shimmer'} />
+        <div className={'w-10 h-10 rounded-full bg-gray-300 animate-pulse'} />
         <div className={'space-y-3'}>
-          <div className={'w-28 h-3 rounded-lg shimmer'} />
-          <div className={'w-20 h-3 rounded-lg shimmer'} />
+          <div className={'w-28 h-3 rounded-lg bg-gray-300 animate-pulse'} />
+          <div className={''} />
         </div>
       </div>
     </div>
