f8ec8f6cebc58f15b9cd8f541d9306f67e68d834
Jhv <jimenezjheave123@gmail.com>
2023-02-25 19:08:42 +0800

    add: firebase for storage solution

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIRE_BASE_API_KEY,
+  authDomain: process.env.FIRE_BASE_AUTH_DOMAIN,
+  projectId: process.env.FIRE_BASE_PROJECT_ID,
+  storageBucket: process.env.FIRE_BASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRE_BASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRE_BASE_APP_ID,
+  measurementId: process.env.FIRE_BASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
