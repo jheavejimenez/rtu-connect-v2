e1dbe24ddbd798fd4ebeac338ae3d861a0da287e
Jhv <jimenezjheave123@gmail.com>
2023-02-25 23:00:57 +0800

    feat: profile upload integration with firebase

--- src/components/Navbar/Login/New/NewProfile.js
+++ src/components/Navbar/Login/New/NewProfile.js
@@ -1,13 +1,39 @@
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import { useAccount } from 'wagmi';
 
+import { storage } from '../../../../utils/firebase';
 import Button from '../../../UI/Button';
 
 function NewProfile({ isModal = false }) {
-  const [avatar, setAvatar] = useState('');
+  const [avatar, setAvatar] = useState(null);
   const [uploading, setUploading] = useState(false);
   const { address } = useAccount();
+  const [file, setFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    const reader = new FileReader();
+    reader.onload = () => {
+      setPreviewUrl(reader.result);
+    };
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const uploadFile = () => {
+    if (avatar == null) {
+      return;
+    }
+    const imageRef = ref(storage, `profile/${file.name}`);
+    uploadBytes(imageRef, file).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -23,23 +49,35 @@ function NewProfile({ isModal = false }) {
     <form onSubmit={formik.handleSubmit}>
       <div className={'flex flex-col px-12 pb-12'}>
         <div className={'flex flex-col items-center justify-center'}>
-          <label
-            className={
-              'flex flex-col items-center justify-center w-32 h-32 border-2 border-gray-300 rounded-full cursor-pointer' +
-              ' hover:bg-gray-100 hover:border-gray-400 bg-gray-50'
-            }
-          >
-            <input
-              type={'file'}
-              className={'hidden'}
-              onChange={(e) => {
-                if (e.target.files[0]) {
-                  setAvatar(e.target.files[0]);
-                }
-              }}
-            />
-            {'Upload Avatar'}
-          </label>
+          {previewUrl ? (
+            <div className={'relative'}>
+              <img
+                className={'w-32 h-32 rounded-full cursor-pointer'}
+                src={previewUrl}
+                alt={'Image Preview'}
+              />
+              <div className={'absolute inset-0 flex items-center justify-center'}>
+                <button
+                  className={
+                    'flex items-center justify-center w-20 opacity-0 font-bold text-red-500 hover:opacity-100 rounded bg-red-100'
+                  }
+                  onClick={() => setPreviewUrl(null)}
+                >
+                  {'remove'}
+                </button>
+              </div>
+            </div>
+          ) : (
+            <label
+              className={
+                'flex flex-col items-center justify-center w-32 h-32 border-2 border-gray-300 rounded-full cursor-pointer' +
+                ' hover:bg-gray-100 hover:border-gray-400 bg-gray-50'
+              }
+            >
+              <input type={'file'} accept={'image/*'} className={'hidden'} onChange={handleFileChange} />
+              {'Upload Avatar'}
+            </label>
+          )}
         </div>
         <input
           id={'handle'}
@@ -55,7 +93,9 @@ function NewProfile({ isModal = false }) {
             ' focus:ring-blue-500 focus:border-blue-500 focus:border-2 sm:text-sm'
           }
         />
-        <Button type={'submit'}>{'Create'}</Button>
+        <Button type={'submit'} onClick={uploadFile}>
+          {'Create'}
+        </Button>
       </div>
     </form>
   );
