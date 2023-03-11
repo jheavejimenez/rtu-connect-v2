e13dcf4691ae01088bec46ca20244eb0d022b1bf
Jhv <jimenezjheave123@gmail.com>
2023-03-12 01:47:04 +0800

    fix: profile query

--- src/components/Profile/Feed.js
+++ src/components/Profile/Feed.js
@@ -12,17 +12,59 @@ import ErrorMessage from '../UI/ErrorMesssage';
 function ProfileFeed({ profile }) {
   const currentProfile = useAppStore((state) => state.currentProfile);
 
-  const profileId = currentProfile?.id;
-  const request = { profileId, limit: 50 };
+  const publicationsRequest = {
+    profileId: currentProfile?.id ?? profile?.id,
+    publicationTypes: ['POST', 'COMMENT', 'MIRROR'],
+    limit: 10
+  };
 
-  const { data, fetchMore, loading, error } = useQuery(PROFILE_FEED, {
-    variables: { request }
+  const { data, loading, error, fetchMore } = useQuery(PROFILE_FEED, {
+    variables: { publicationsRequest }
   });
 
-  const publications = data?.feed?.items;
-  const pageInfo = data?.feed?.pageInfo;
-  const hasMore = pageInfo?.next && publications?.length !== pageInfo.totalCount;
-  const loadMore = async () => {};
+  const publications = data?.publications?.items;
+  const pageInfo = data?.publications?.pageInfo;
+  /**
+   * TODO: pageInfo.totalCount is null for some reason so we can't use it
+   const hasMore = pageInfo?.next && publications?.length !== pageInfo.totalCount;
+   **/
+
+  /**
+   * remember to fix this when the pageInfo.totalCount
+   * is fixed for now we limit the number of publications to 100 to avoid crashing the browser
+   */
+  const hasMore = pageInfo?.next && publications?.length < 100;
+
+  const loadMore = async () => {
+    const loadedIds = new Set();
+    const updateQuery = (prev, { fetchMoreResult }) => {
+      if (!fetchMoreResult) {
+        return prev;
+      }
+      const newData = fetchMoreResult.publications.items.filter((item) => !loadedIds.has(item.id));
+      for (const item of newData) {
+        loadedIds.add(item.id);
+      }
+      return {
+        publications: {
+          ...fetchMoreResult.publications,
+          items: [...prev.publications.items, ...newData]
+        }
+      };
+    };
+    if (!hasMore) {
+      return;
+    }
+    await fetchMore({
+      variables: {
+        request: {
+          ...publicationsRequest,
+          cursor: pageInfo?.next
+        }
+      },
+      updateQuery
+    });
+  };
 
   if (loading) {
     return <FeedShimmer />;
@@ -33,6 +75,7 @@ function ProfileFeed({ profile }) {
   if (error) {
     return <ErrorMessage title={`Failed to load feed`} error={error} />;
   }
+  console.log(publications);
   return (
     <InfiniteScroll
       dataLength={publications?.length ?? 0}
@@ -51,11 +94,7 @@ function ProfileFeed({ profile }) {
       }
     >
       {publications?.map((publication, index) => (
-        <SinglePublication
-          key={`${publication?.root.id}_${index}`}
-          feedItem={publication}
-          publication={publication.root}
-        />
+        <SinglePublication key={`${publication.id}_${index}`} publication={publication} />
       ))}
     </InfiniteScroll>
   );
