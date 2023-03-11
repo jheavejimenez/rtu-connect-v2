a5d103f3d29a904363de1130815c7b3c3562c3c8
Jhv <jimenezjheave123@gmail.com>
2023-03-12 01:47:51 +0800

    feat: watch feed

---
+++ src/components/Watch/index.js
@@ -0,0 +1,20 @@
+import BetaWarning from '../Home/BetaWarning';
+import RecommendedProfile from '../Home/RecommendedProfile';
+import { GridLayout } from '../UI/GridLayout';
+import WatchFeed from './Feed';
+
+function Watch() {
+  return (
+    <GridLayout>
+      <div className={'space-y-5 lg:col-span-8 md:col-span-12 col-span-12 mb-5'}>
+        <WatchFeed feedType={'LATEST'} />
+      </div>
+      <div className={'lg:col-span-4 md:col-span-12 col-span-12'}>
+        <BetaWarning />
+        <RecommendedProfile />
+      </div>
+    </GridLayout>
+  );
+}
+
+export default Watch;
