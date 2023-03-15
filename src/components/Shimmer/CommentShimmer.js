b78ebfaa2aded6cd6965aec5da85b70b8f33403e
Jhv <jimenezjheave123@gmail.com>
2023-03-15 18:37:22 +0800

    fix: comment shimmer

---
+++ src/components/Shimmer/CommentShimmer.js
@@ -0,0 +1,12 @@
+import PublicationShimmer from './PublicationShimmer';
+
+function CommentShimmer() {
+  return (
+    <>
+      <PublicationShimmer />
+      <PublicationShimmer />
+    </>
+  );
+}
+
+export default CommentShimmer;
