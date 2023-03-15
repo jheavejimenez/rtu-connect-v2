ecc8ce926105072bf2c4580da45cfb3a81880795
Charlon-Dale <charlondalesanagustin@gmail.com>
2023-03-15 20:58:38 +0800

    fix the allignment and display of navItems

--- src/components/Navbar/NavItems.js
+++ src/components/Navbar/NavItems.js
@@ -3,12 +3,12 @@ import Link from 'next/link';
 function NavItems({ url, name, current, logo }) {
   return (
     <Link href={url} aria-current={current ? 'page' : undefined}>
-      <li className={'w-1/5 md:w-max text-center'}>
+      <li className={' w-1/5 md:w-max text-center '}>
         <div
-          className={`${current && 'bg-gray-200'}
+          className={`${current}
             inline-block rounded text-gray-600 w-full 
             text-md py-2 px-3 xl:px-12 cursor-pointer 
-            hover:bg-gray-100 relative`}
+            hover:bg-gray-200 relative`}
         >
           {logo}
         </div>
