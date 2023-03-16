a3f9faae8f0c16118945b0b3d61c1df72547c74f
Charlon-Dale <charlondalesanagustin@gmail.com>
2023-03-17 01:26:35 +0800

    fixed the logic in circuit operator in current

--- src/components/Navbar/NavItems.js
+++ src/components/Navbar/NavItems.js
@@ -5,7 +5,7 @@ function NavItems({ url, name, current, logo }) {
     <Link href={url} aria-current={current ? 'page' : undefined}>
       <li className={'w-max md:w-max text-center flex justify-center'}>
         <div
-          className={`${current && 'text-blue-800'} ${current && 'bg-gray-200'}
+          className={`${current && 'bg-gray-200 text-blue-800'}
             inline-block rounded text-gray-600 w-full 
             text-md py-2 px-3 xl:px-12 cursor-pointer 
             hover:bg-gray-200 hover:text-blue-800 relative`}
