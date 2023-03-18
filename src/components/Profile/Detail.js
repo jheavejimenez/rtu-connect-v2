f6a535218747b6f46128046be3014842dda11455
Jhv <jimenezjheave123@gmail.com>
2023-03-19 01:21:32 +0800

    feat: data for detail

--- src/components/Profile/Detail.js
+++ src/components/Profile/Detail.js
@@ -1,4 +1,7 @@
-function Detail() {
+import { fixUsername, getAvatarUrl } from '../../utils/helpers';
+import Button from '../UI/Button';
+
+function Detail({ profile }) {
   return (
     <aside
       className={
@@ -7,25 +10,40 @@ function Detail() {
       }
     >
       <div className={'flex flex-col gap-1 text-center items-center'}>
-        <img className={'h-28 w-28 bg-white p-2 rounded-full shadow mb-4'} alt={'user avatar'} />
+        <img
+          className={'h-28 w-28 bg-white p-2 rounded-full shadow mb-4'}
+          src={getAvatarUrl(profile)}
+          alt={'user avatar'}
+        />
       </div>
       <div className={'flex items-center justify-center'}>
-        <span className={'font-semibold'}>{'@testdarell1.rtu'} </span>
+        <span className={'font-semibold'}>{fixUsername(profile?.name) ?? fixUsername(profile?.handle)}</span>
       </div>
       <div className={'flex items-center justify-center'}>
-        <span className={'text-sm truncate'}>
-          {'Wallet Adress: 0x014A0C35D33683BC3e50F18AE669432D7e49A13d'}
-        </span>
+        {profile?.ownedBy !== undefined ? (
+          <span className={'text-sm text-gray-400'}>{`Wallet Address: ${profile?.ownedBy.slice(
+            0,
+            6
+          )}...${profile?.ownedBy.slice(-4)}`}</span>
+        ) : (
+          <span className={'text-sm animate-pulse text-blue-900'}>{'...loading'}</span>
+        )}
       </div>
       <div className={'flex justify-center items-center gap-2 my-3'}>
         <div className={'font-semibold text-center mx-4'}>
-          <p className={'text-black'}>{' 0'}</p>
+          <p className={'text-black'}>{profile?.stats?.totalFollowers}</p>
           <span className={'text-gray-400'}>{'Followers'}</span>
         </div>
         <div className={'font-semibold text-center mx-4'}>
-          <p className={'text-black'}>{'0'}</p>
+          <p className={'text-black'}>{profile?.stats?.totalFollowing}</p>
           <span className={'text-gray-400'}>{'Following'}</span>
         </div>
+      </div>
+      <div className={'flex items-center justify-center pt-3'}>
+        <Button>{'Follow'}</Button>
+      </div>
+      <div className={'flex items-center justify-center p-3'}>
+        <span className={'text-sm text-gray-400 whitespace-pre-wrap break-words'}>{profile?.bio}</span>
       </div>
     </aside>
   );
