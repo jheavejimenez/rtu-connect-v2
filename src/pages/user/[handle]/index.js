25c1d7ffa6d7e79e400793b8dca2ea5479a6c1f9
Jhv <jimenezjheave123@gmail.com>
2023-03-19 01:21:49 +0800

    add beta warning

--- src/pages/user/[handle]/index.js
+++ src/pages/user/[handle]/index.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 
+import BetaWarning from '../../../components/Home/BetaWarning';
 import Detail from '../../../components/Profile/Detail';
 import ProfileFeed from '../../../components/Profile/Feed';
 import { GridLayout } from '../../../components/UI/GridLayout';
@@ -37,7 +38,8 @@ function ViewProfile() {
   return (
     <GridLayout>
       <div className={'lg:col-span-4 md:col-span-12 col-span-12'}>
-        <Detail />
+        <Detail profile={profile} />
+        <BetaWarning />
       </div>
       <div className={'space-y-5 lg:col-span-8 md:col-span-12 col-span-12 mb-5'}>
         <ProfileFeed profile={profile} />
