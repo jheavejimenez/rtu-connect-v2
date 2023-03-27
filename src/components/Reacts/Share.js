69668bdc10a3a2c15576d2857dad37acb10856be
Jhv <jimenezjheave123@gmail.com>
2023-03-27 18:03:15 +0800

    feat: share

--- src/components/Reacts/Share.js
+++ src/components/Reacts/Share.js
@@ -1,14 +1,17 @@
-import { useMutation } from '@apollo/client';
+import { useApolloClient, useMutation } from '@apollo/client';
 import { ArrowPathRoundedSquareIcon } from '@heroicons/react/24/outline';
+import { splitSignature } from 'ethers/lib/utils';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useContractWrite, useSignTypedData } from 'wagmi';
 
 import { BROADCAST_TRANSACTION } from '../../graphQL/mutations/broadcast-transaction';
+import { CREATE_MIRROR } from '../../graphQL/mutations/create-mirror';
 import { useAppStore } from '../../store/app';
 import LensHubProxy from '../../utils/abis/LensHubProxy.json';
 import { LENS_HUB_MUMBAI } from '../../utils/constants';
+import { getSignature } from '../../utils/helpers';
 import Spinner from '../UI/Spinner';
 
 function Share({ publication, electedMirror }) {
@@ -21,8 +24,6 @@ function Share({ publication, electedMirror }) {
   const setUserSigNonce = useAppStore((state) => state.setUserSigNonce);
   const currentProfile = useAppStore((state) => state.currentProfile);
 
-  const [isLoading, setIsloading] = useState(false);
-
   const [mirrored, setMirrored] = useState(
     isMirror ? publication?.mirrorOf?.mirrors?.length > 0 : publication?.mirrors?.length > 0
   );
@@ -33,7 +34,9 @@ function Share({ publication, electedMirror }) {
     }
   });
 
-  const updateCache = (cached) => {
+  const { cache } = useApolloClient();
+
+  const updateCache = () => {
     cache.modify({
       id: isMirror ? publication?.mirrorOf : publication,
       fields: {
@@ -45,14 +48,11 @@ function Share({ publication, electedMirror }) {
     });
   };
 
-  const { write } = useContractWrite({
+  const { isLoading: writeLoading, write } = useContractWrite({
     address: LENS_HUB_MUMBAI,
     abi: LensHubProxy,
     functionName: 'mirrorWithSig',
     mode: 'recklesslyUnprepared',
-    onSuccess: () => {
-      toast('Post shared!');
-    },
     onError: (error) => {
       toast(`Error sharing post: ${error.message}`);
     }
@@ -61,17 +61,72 @@ function Share({ publication, electedMirror }) {
   const [broadcast, { loading: broadcastLoading }] = useMutation(BROADCAST_TRANSACTION, {
     onCompleted: () => {
       setMirrored(true);
-      toast.success('Mirrored!');
+      toast.success('Post shared successfully');
     },
     update: updateCache
   });
 
-  function createMirror() {
-    setIsloading(true);
-    setTimeout(() => {
-      setIsloading(false);
-    }, 1000);
-  }
+  const [createMirrorTypedData, { loading: typedDataLoading }] = useMutation(CREATE_MIRROR, {
+    onCompleted: async ({ createMirrorTypedData }) => {
+      const { id, typedData } = createMirrorTypedData;
+      const {
+        profileId,
+        profileIdPointed,
+        pubIdPointed,
+        referenceModule,
+        referenceModuleData,
+        referenceModuleInitData,
+        deadline
+      } = typedData.value;
+      const signature = await signTypedDataAsync(getSignature(typedData));
+      const { v, r, s } = splitSignature(signature);
+      const sig = { v, r, s, deadline };
+      const inputStruct = {
+        profileId,
+        profileIdPointed,
+        pubIdPointed,
+        referenceModule,
+        referenceModuleData,
+        referenceModuleInitData,
+        sig
+      };
+      setUserSigNonce(userSigNonce + 1);
+      const { data } = await broadcast({ variables: { request: { id, signature } } });
+      if (data?.broadcast.__typename === 'RelayError') {
+        return write?.({ recklesslySetUnpreparedArgs: [inputStruct] });
+      }
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    }
+  });
+
+  const createMirror = async () => {
+    if (!currentProfile) {
+      return toast.error('Please connect your wallet');
+    }
+
+    try {
+      const request = {
+        profileId: currentProfile?.id,
+        publicationId: publication?.id,
+        referenceModule: {
+          followerOnlyReferenceModule: false
+        }
+      };
+
+      return await createMirrorTypedData({
+        variables: {
+          options: { overrideSigNonce: userSigNonce },
+          request
+        }
+      });
+    } catch {
+      toast.error('Error creating mirror');
+    }
+  };
+
+  const isLoading = typedDataLoading || signLoading || writeLoading || broadcastLoading;
 
   return (
     <motion.button
@@ -83,11 +138,11 @@ function Share({ publication, electedMirror }) {
       <span className={'text-blue-500 flex items-center space-x-1'}>
         <span
           className={
-            isMirror ? 'hover:bg-green-300' : 'hover:bg-blue-300 p-1.5 rounded-full hover:bg-opacity-20'
+            mirrored ? 'hover:bg-green-300' : 'hover:bg-blue-300 p-1.5 rounded-full hover:bg-opacity-20'
           }
         >
           {isLoading ? (
-            <Spinner variant={isMirror ? 'success' : 'primary'} size={'xs'} />
+            <Spinner variant={mirrored ? 'success' : 'primary'} size={'xs'} />
           ) : (
             <ArrowPathRoundedSquareIcon className={'w-[15px] sm:w-[18px]'} />
           )}
