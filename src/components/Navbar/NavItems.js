cc7492a7b395707df0edeb2a0cb86fd44c3caff7
Charlon-Dale <charlondalesanagustin@gmail.com>
2023-04-02 20:28:05 +0800

    revert the changes in the navitems

--- src/components/Navbar/NavItems.js
+++ src/components/Navbar/NavItems.js
@@ -5,7 +5,7 @@ function NavItems({ url, name, current, logo }) {
     <Link href={url} aria-current={current ? 'page' : undefined}>
       <li className={'w-max text-center flex justify-center'}>
         <div
-          className={`${current && 'text-blue-700'}
+          className={`${current && 'bg-gray-200 text-blue-800'}
             inline-block rounded text-gray-600 w-full 
             text-md py-2 px-12 cursor-pointer 
             hover:bg-gray-200 hover:text-blue-800 relative`}
